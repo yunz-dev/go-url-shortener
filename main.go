349c77d6754735a54c721c22ad6750815710f706
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-09 23:22:21 +1100

    added: function to generate random shortened string

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+  "math/rand"
 	"time"
 )
 
@@ -12,6 +13,23 @@ type Response struct {
 	Time    string `json:"time"`
 }
 
+urls := make(map[string]string)
+
+// GenerateRandomString generates a random string of the given length.
+func GenerateRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+func ShortenHandler(url: string) {
+  urls[string] := GenerateRandomString(8)
+  return urls[string]
+}
+
 // main initializes and starts the server
 func main() {
 	mux := http.NewServeMux()
@@ -19,6 +37,7 @@ func main() {
 	// Register handlers
 	mux.HandleFunc("/hello", handlers.HelloHandler)
 	mux.HandleFunc("/", handlers.NotFoundHandler)
+  mux.HandleFunc("/shorten/", ShortenHandler)
 
 	// Start server
 	server := &http.Server{
